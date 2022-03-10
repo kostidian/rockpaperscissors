570e66f52b1319de5ea9d2ea9c981c6480242f13
kostidian <markjcosta@gmail.com>
2022-03-10 15:54:14 +1100

    start

---
+++ gameplay.js
@@ -0,0 +1,20 @@
+function computerPlay() {
+    let compchoice = Math.floor((Math.random() * 3) +1);
+    if (compchoice == 1) {
+        compchoice = "Rock"
+    } else if (compchoice ==2) {
+        compchoice = "Paper"
+    } else {
+        compchoice = "Scissors"
+    }
+    console.log(compchoice)
+}
+
+function playRound() {
+
+}
+
+function game() {
+
+}
+
